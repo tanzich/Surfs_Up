f62aab1f25b954cd358f6379e34f67314e22dec3
Tanzia Chaudhury <Tanzich@tanzias-mbp.lan>
2022-01-30 01:30:30 -0800

    surfup analysis

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Surfs_Up
+
+## Overview of the statistical analysis
+### Purpose
+This analysis is done for W. Avy to provide him with more information about the temperature trends before opening the surf shop.
+
+## Results
+Three key differences in weather between June and December are: 
+
+- The average recorded temperature for the month of June is 74.94 degrees F, where as, The average recorded temperature for the month of December is 71.04 degrees F
+
+- The max temp recorded is 85 degrees F in June and 83 degrees F in Dec
+
+- The min temp recorded is 64 degrees F in June and 56 degrees F in Dec
+
+![summaryTemp](June_Dec_Temp_Summary.png)
+
+## Summary
+The weather in June compared to the weather in December seems to vary, however, both months in my opinion, would have a good weather conditions for surfing. 
+
+### Additional Queries to gather more weather data for June and December
+
+It would nice to gather more data to get further information. For example, 
+
+- If we can query the temperature based on the station location, we would know which station will be a petter location for the shop
+
+- We could also create a query to get precipitation information to determine whether it plays a factor despite the warm temperatures that are experienced during this time.
---
+++ app.py
@@ -0,0 +1,88 @@
+# Dependencies and Setup
+import numpy as np
+import datetime as dt
+
+# Python SQL Toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.pool import StaticPool
+
+# Import Flask
+from flask import Flask, jsonify
+
+# Set Up the Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+# Define what to do when a user hits the index route
+@app.route('/')
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+if __name__ == "__main__":
+    app.run(debug=True)
